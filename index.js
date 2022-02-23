a52e2cc9f060e0ba8f8c5ae035e86f9879847e06
kazinayeem <mohammadalinayeem360@gmail.com>
2022-02-23 19:04:01 +0600

    make it better

---
+++ index.js
@@ -0,0 +1,93 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient, ObjectId } from "mongodb";
+
+const url =
+  "mongodb+srv://admin:admin123@cluster0.j0gm6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const funconnecteddata = async () => {
+  try {
+    await client
+      .connect()
+      .then((res) => {
+        console.log("Connected");
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+
+    //    database
+    const database = client.db("testing");
+    const todo = database.collection("todo");
+
+    // routing
+    app.post("/", async (req, res) => {
+      const todos = req.body;
+
+      try {
+        await todo.insertOne(todos);
+
+        return res.status(200).json({
+          message: "insert successfull",
+        });
+      } catch (error) {
+        return res.status(500).json({
+          message: "error",
+        });
+      }
+    });
+
+    app.get("/", async (req, res) => {
+      try {
+        const data = await todo.find({});
+        const result = await data.toArray();
+        res.send(result);
+      } catch (error) {
+        return res.status(500).json({
+          message: "error",
+        });
+      }
+    });
+
+    app.get("/:id", async (req, res) => {
+      const id = ObjectId(req.params.id);
+      try {
+        const data = await todo.find({ _id: id });
+        const result = await data.toArray();
+        res.send(result);
+      } catch (error) {
+        return res.status(500).json({
+          message: "error",
+        });
+      }
+    });
+
+    app.delete("/:id", async (req, res) => {
+      const id = ObjectId(req.params.id);
+      try {
+        await todo.deleteOne({ _id: id });
+
+        res.status(200).json({
+          result: "delete successfull",
+        });
+      } catch (error) {
+        return res.status(500).json({
+          message: "error",
+        });
+      }
+    });
+  } catch (error) {
+    console.log("error: ", error);
+  }
+};
+
+funconnecteddata();
+app.listen();
